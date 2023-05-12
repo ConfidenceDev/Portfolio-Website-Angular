93fec0bde95d40cc562d5f4b5d54074b7c16b36c
ConfidenceDev <confidostic3@gmail.com>
2023-05-12 15:26:06 +0100

    Ongoing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  @Output() btnClick = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onClick() {
+    this.btnClick.emit();
+  }
+}
